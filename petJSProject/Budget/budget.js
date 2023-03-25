0b829b83a997b25a2ba9166c92cff786fd788e32
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2023-03-25 20:04:24 +0700

    js pj

---
+++ petJSProject/Budget/budget.js
@@ -0,0 +1,50 @@
+let totalAmount = document.getElementById("total-amount");
+let userAmount = document.getElementById("");
+const checkAmountButton = document.getElementById("check-amount");
+const totalAmountButton = document.getElementById("total-amount-button");
+const productTitle = document.getElementById("product-title");
+const errorMessage = document.getElementById("budget-error");
+const productTitleError = document.getElementById("product-title-error");
+const productCostError = document.getElementById("product-cost-error");
+const amount = document.getElementById("amount");
+const expenditureValue = document.getElementById("expenditure-value");
+const balanceValue = document.getElementById("balance-amount");
+
+let tempAmount = 0;
+totalAmountButton.addEventListener("click", () => {
+    tempAmount = totalAmount.value;
+    if (tempAmount === "" || tempAmount < 0) {
+        errorMessage.classList.remove("hide");
+    } else {
+        errorMessage.classList.add("hide");
+        amount.innerHTML = tempAmount;
+        balanceValue.innerText = tempAmount - expenditureValue.innerText;
+        totalAmount.value = 0;
+    };
+});
+
+const disableButtons = (bool) => {
+    let editButtons = document.getElementsByClassName("edit");
+    Array.from(editButtons).forEach((element) => {
+        element.disabled = bool;
+    });
+};
+
+const modifyElement = (element, edit = false) => {
+    let parentDiv = element.parentElement;
+    let currentBalance = balanceValue.innerText;
+    let currentExpence = expenditureValue.innerText;
+    let parentAmount = parentDiv.querySelector(".amount").innerText;
+
+    if (edit) {
+        let parentText = parentDiv.querySelector(".product").innerText;
+        productTitle.value = parentText;
+        userAmount.value = parentAmount;
+        disableButtons(true);
+    };
+
+    balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
+    expenditureValue.innerText = parseInt(currentExpence) - parseInt(parentAmount);
+    parentDiv.remove();
+};
+
